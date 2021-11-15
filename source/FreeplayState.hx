9c9c1acb525873b5a8638a872ea126694634988f
NotSpringtrap <poketapugxyt@gmail.com>
2021-11-15 21:05:38 +0100

    Initial commit

---
+++ source/FreeplayState.hx
@@ -0,0 +1,370 @@
+package;
+
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+
+
+#if windows
+import Discord.DiscordClient;
+#end
+
+using StringTools;
+
+class FreeplayState extends MusicBeatState
+{
+    var songs:Array<SongMetadata> = [];
+
+    var selector:FlxText;
+    var curSelected:Int = 0;
+    var curDifficulty:Int = 1;
+    var freeplayBG:FlxSprite;
+    var bg:FlxSprite;
+    var scoreText:FlxText;
+    var diffText:FlxText;
+    var lerpScore:Int = 0;
+    var intendedScore:Int = 0;
+
+    private var grpSongs:FlxTypedGroup<Alphabet>;
+    private var curPlaying:Bool = false;
+
+    private var iconArray:Array<HealthIcon> = [];
+
+    override function create()
+    {
+        var initSonglist = CoolUtil.coolTextFile(Paths.txt('freeplaySonglist'));
+
+        for (i in 0...initSonglist.length)
+        {
+            var data:Array<String> = initSonglist[i].split(':');
+            songs.push(new SongMetadata(data[0], Std.parseInt(data[2]), data[1]));
+        }
+
+        /*
+            if (FlxG.sound.music != null)
+            {
+                if (!FlxG.sound.music.playing)
+                    FlxG.sound.playMusic(Paths.music('freakyMenu'));
+            }
+         */
+
+         #if windows
+         // Updating Discord Rich Presence
+         DiscordClient.changePresence("In the Freeplay Menu", null);
+         #end
+
+        var isDebug:Bool = false;
+
+        #if debug
+        isDebug = true;
+        #end
+
+        // LOAD MUSIC
+
+        // LOAD CHARACTERS
+
+        var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBGBlue'));
+        bg.scrollFactor.x = 0;
+        bg.scrollFactor.y = 0.18;
+        bg.setGraphicSize(Std.int(bg.width * 1.1));
+        bg.updateHitbox();
+        bg.screenCenter();
+        bg.antialiasing = true;
+        add(bg);
+
+		bg = new FlxSprite(580, -10);
+        bg.frames = Paths.getSparrowAtlas('logoBumpin');
+        bg.animation.addByPrefix('bump', 'logo bumpin', 24, true);
+        bg.animation.play('bump');
+        bg.antialiasing = true;
+        bg.active = true;
+        bg.scrollFactor.x = 580;
+        bg.scrollFactor.y = -10;
+        bg.setGraphicSize(Std.int(bg.width * 0.75));
+        bg.updateHitbox();
+        add(bg);
+        
+        grpSongs = new FlxTypedGroup<Alphabet>();
+        add(grpSongs);
+
+        for (i in 0...songs.length)
+        {
+            var songText:Alphabet = new Alphabet(0, (70 * i) + 30, songs[i].songName, true, false);
+            songText.isMenuItem = true;
+
+
+            grpSongs.add(songText);
+
+            var icon:HealthIcon = new HealthIcon(songs[i].songCharacter);
+            icon.sprTracker = songText;
+
+            // using a FlxGroup is too much fuss!
+            iconArray.push(icon);
+            add(icon);
+
+            songText.x += 40;
+            // DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
+        }
+
+        scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
+        // scoreText.autoSize = false;
+        scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
+        // scoreText.alignment = RIGHT;
+
+        var scoreBG:FlxSprite = new FlxSprite(scoreText.x - 6, 0).makeGraphic(Std.int(FlxG.width * 0.35), 66, 0xFF000000);
+        scoreBG.alpha = 0.6;
+        add(scoreBG);
+
+        diffText = new FlxText(scoreText.x, scoreText.y + 36, 0, "", 24);
+        diffText.font = scoreText.font;
+        add(diffText);
+
+        add(scoreText);
+
+        changeSelection();
+        changeDiff();
+
+        // FlxG.sound.playMusic(Paths.music('title'), 0);
+        // FlxG.sound.music.fadeIn(2, 0, 0.8);
+        selector = new FlxText();
+
+        selector.size = 40;
+        selector.text = ">";
+        // add(selector);
+
+        var swag:Alphabet = new Alphabet(1, 0, "swag");
+
+        // JUST DOIN THIS SHIT FOR TESTING!!!
+        /*
+            var md:String = Markdown.markdownToHtml(Assets.getText('CHANGELOG.md'));
+
+            var texFel:TextField = new TextField();
+            texFel.width = FlxG.width;
+            texFel.height = FlxG.height;
+            // texFel.
+            texFel.htmlText = md;
+
+            FlxG.stage.addChild(texFel);
+
+            // scoreText.textField.htmlText = md;
+
+            trace(md);
+         */
+
+        super.create();
+    }
+
+    public function addSong(songName:String, weekNum:Int, songCharacter:String)
+    {
+        songs.push(new SongMetadata(songName, weekNum, songCharacter));
+    }
+
+    public function addWeek(songs:Array<String>, weekNum:Int, ?songCharacters:Array<String>)
+    {
+        if (songCharacters == null)
+            songCharacters = ['bf'];
+
+        var num:Int = 0;
+        for (song in songs)
+        {
+            addSong(song, weekNum, songCharacters[num]);
+
+            if (songCharacters.length != 1)
+                num++;
+        }
+    }
+
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+        if (FlxG.sound.music.volume < 0.7)
+        {
+            FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+        }
+
+        lerpScore = Math.floor(FlxMath.lerp(lerpScore, intendedScore, 0.4));
+
+        if (Math.abs(lerpScore - intendedScore) <= 10)
+            lerpScore = intendedScore;
+
+        scoreText.text = "PERSONAL BEST:" + lerpScore;
+
+        var upP = controls.UP_P;
+        var downP = controls.DOWN_P;
+        var accepted = controls.ACCEPT;
+
+        if (upP)
+        {
+            changeSelection(-1);
+        }
+        if (downP)
+        {
+            changeSelection(1);
+        }
+
+        if (controls.LEFT_P)
+            changeDiff(-1);
+        if (controls.RIGHT_P)
+            changeDiff(1);
+
+        if (controls.BACK)
+        {
+            FlxG.switchState(new MainMenuState());
+        }
+
+        if (accepted)
+        {
+            var poop:String = Highscore.formatSong(songs[curSelected].songName.toLowerCase(), curDifficulty);
+
+            trace(poop);
+
+            PlayState.SONG = Song.loadFromJson(poop, songs[curSelected].songName.toLowerCase());
+            PlayState.isStoryMode = false;
+            PlayState.storyDifficulty = curDifficulty;
+            PlayState.storyWeek = songs[curSelected].week;
+            trace('CUR WEEK' + PlayState.storyWeek);
+            LoadingState.loadAndSwitchState(new PlayState());
+        }
+    }
+
+    function changeDiff(change:Int = 0)
+    {
+        curDifficulty += change;
+
+        if (curDifficulty < 0)
+            curDifficulty = 3;
+        if (curDifficulty > 3)
+            curDifficulty = 0;
+
+        #if !switch
+        intendedScore = Highscore.getScore(songs[curSelected].songName, curDifficulty);
+        #end
+
+        switch (curDifficulty)
+        {
+            case 0:
+                diffText.text = "EASY";
+            case 1:
+                diffText.text = 'NORMAL';
+            case 2:
+                diffText.text = "HARD";
+            case 3:
+                diffText.text = "CORRUPT";
+        }
+    }
+
+    function changeSelection(change:Int = 0)
+    {
+        #if !switch
+        // NGio.logEvent('Fresh');
+        #end
+
+        // NGio.logEvent('Fresh');
+        FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+        curSelected += change;
+
+        if (curSelected < 0)
+            curSelected = songs.length - 1;
+        if (curSelected >= songs.length)
+            curSelected = 0;
+        /*if (curSelected <= 1)
+            freeplayBG.animation.play('Week5');
+        if (curSelected == 2)
+            freeplayBG.animation.play('WinterHorrorland');
+        if (curSelected == 3)
+            freeplayBG.animation.play('Pico');
+        if (curSelected == 4)
+            freeplayBG.animation.play('Philly');
+        if (curSelected == 5)
+            freeplayBG.animation.play('Blammed');
+        if (curSelected == 6)
+            freeplayBG.animation.play('Spookeez');
+        if (curSelected == 7)
+            freeplayBG.animation.play('SanguineSouth');
+        if (curSelected == 8)
+            freeplayBG.animation.play('Chiller');
+        if (curSelected == 9)
+            freeplayBG.animation.play('Matricidal');
+        if (curSelected == 10)
+            freeplayBG.animation.play('Milk');
+        if (curSelected == 11)
+            freeplayBG.animation.play('Schizophrenzy');
+        if (curSelected == 12)
+            freeplayBG.animation.play('Senpai');
+        if (curSelected == 13)
+            freeplayBG.animation.play('DeadPixel');
+        if (curSelected == 14)
+            freeplayBG.animation.play('TreacherousThorns');
+        if (curSelected == 15)
+            freeplayBG.animation.play('Roots');
+        if (curSelected == 16)
+            freeplayBG.animation.play('Lament');
+        if (curSelected == 17)
+            freeplayBG.animation.play('Dusk');
+        if (curSelected == 18)
+            freeplayBG.animation.play('Deathmatch');
+        if (curSelected == 19)
+            freeplayBG.animation.play('Tormentor');
+        if (curSelected == 20)
+            freeplayBG.animation.play('Tormentor');
+        if (curSelected == 21)
+            freeplayBG.animation.play('Chiller');*/
+
+        // selector.y = (70 * curSelected) + 30;
+
+        #if !switch
+        intendedScore = Highscore.getScore(songs[curSelected].songName, curDifficulty);
+        // lerpScore = 0;
+        #end
+
+        #if PRELOAD_ALL
+        FlxG.sound.playMusic(Paths.inst(songs[curSelected].songName), 0);
+        #end
+
+        var bullShit:Int = 0;
+
+        for (i in 0...iconArray.length)
+        {
+            iconArray[i].alpha = 0.6;
+        }
+
+        iconArray[curSelected].alpha = 1;
+
+        for (item in grpSongs.members)
+        {
+            item.targetY = bullShit - curSelected;
+            bullShit++;
+
+            item.alpha = 0.6;
+            // item.setGraphicSize(Std.int(item.width * 0.8));
+
+            if (item.targetY == 0)
+            {
+                item.alpha = 1;
+                // item.setGraphicSize(Std.int(item.width));
+            }
+        }
+    }
+}
+
+class SongMetadata
+{
+    public var songName:String = "";
+    public var week:Int = 0;
+    public var songCharacter:String = "";
+
+    public function new(song:String, week:Int, songCharacter:String)
+    {
+        this.songName = song;
+        this.week = week;
+        this.songCharacter = songCharacter;
+    }
+}
